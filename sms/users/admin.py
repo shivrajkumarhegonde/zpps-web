4ccb7fbd9a60d7b78e52dd13851e50ce12121b8f
shivrajkumarhegonde <shivrajkumarhegonde@gmail.com>
2025-01-27 13:12:30 +0530

    minor changes

--- sms/users/admin.py
+++ sms/users/admin.py
@@ -1,3 +1,11 @@
+# users/admin.py
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'is_staff', 'is_active']
